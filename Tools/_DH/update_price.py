b6d5df7e4f8e58fbfa4a3e6de2a7e5a5f3a27c31
Darkiich <kurachiy75@email.com>
2026-01-10 19:53:14 +0300

    Обновил скрипт обновления цен и обновил цены карт

--- Tools/_DH/update_price.py
+++ Tools/_DH/update_price.py
@@ -14,21 +14,24 @@ for name, val in matches:
     int_price = int(float_val)
     price_map[name] = int_price
 
-# Директория с файлами, где надо изменить
-directory = ''
+list_of_directory_ship = ["_DH", "_Lua", "_Mono", "_NF"]
 
-for root, _, files in os.walk(directory):
-    for file in files:
-        if file.endswith(('.yml', '.yaml')):
-            path = os.path.join(root, file)
-            with open(path, 'r+', encoding='utf-8') as f:
-                content = f.read()
-                updated = False
-                for name, price in price_map.items():
-                    if f"id: {name}" in content:
-                        content = re.sub(r"price:\s*\d+", f"price: {price}", content)
-                        updated = True
-                if updated:
-                    f.seek(0)
-                    f.write(content)
-                    f.truncate()
+# Директория с файлами, где надо изменить
+for path in list_of_directory_ship:
+    directory = f'Resources/Prototypes/{path}/Shipyard'
+
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(('.yml', '.yaml')):
+                path = os.path.join(root, file)
+                with open(path, 'r+', encoding='utf-8') as f:
+                    content = f.read()
+                    updated = False
+                    for name, price in price_map.items():
+                        if f"id: {name}" in content:
+                            content = re.sub(r"price:\s*\d+", f"price: {price}", content)
+                            updated = True
+                    if updated:
+                        f.seek(0)
+                        f.write(content)
+                        f.truncate()
